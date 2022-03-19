8ebf22ff02e5d130a9d023be5709c8bc281efb81
Prashant <pk3076889@gmail.com>
2022-03-19 12:29:31 +0530

    commit

--- 1.c
+++ 1.c
@@ -2,13 +2,13 @@
 int compare(char[],char[]);  
 int main()  
 {  
-   char str1[20]; // declaration of char array  
-   char str2[20]; // declaration of char array  
+   char str1[20];   
+   char str2[20];   
    printf("Enter the first string : ");  
    scanf("%s",str1);  
    printf("Enter the second string : ");  
    scanf("%s",str2);  
-   int c= compare(str1,str2); // calling compare() function  
+   int c= compare(str1,str2);  
    if(c==0)  
    printf("strings are same");  
    else  
@@ -17,11 +17,11 @@ int main()
     return 0;  
 }  
   
-// Comparing both the strings.  
+  
 int compare(char a[],char b[])  
 {  
-    int flag=0,i=0;  // integer variables declaration  
-    while(a[i]!='\0' &&b[i]!='\0')  // while loop  
+    int flag=0,i=0;   
+    while(a[i]!='\0' &&b[i]!='\0')    
     {  
        if(a[i]!=b[i])  
        {  
